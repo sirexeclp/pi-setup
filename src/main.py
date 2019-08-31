edee608ed0c237935da0e41483478a8b87b0c218
felix <sirexeclp@gmail.com>
2019-08-31 03:25:15 +0200

    configure ssh and password

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import json
 import subprocess
 import sys
 from pathlib import Path
-
+import crypt, getpass, pwd
 from gevent import os
 
 
@@ -19,6 +19,7 @@ def render_template(template_file, **kwargs):
 def check_path(path):
     """Converts given string to Path object and throws if the path does not exist."""
     path = Path(path)
+    assert path != Path("/"), "Do not use the host root here!"
     if not path.exists():
         raise IOError(f"{path} does not exist")
     return path
@@ -75,17 +76,117 @@ def get_mounts(card, filter):
     return mounts
 
 
+def ssh_keygen(key_name, password):
+    ssh_root = Path.home() / Path(".ssh")
+    ssh_root.mkdir(exist_ok=True, parents=True)
+    filename = ssh_root / key_name
+    process = subprocess.run(["ssh-keygen", "-b", "4096", "-f", filename, "-N", password])
+    return filename.with_suffix(".pub")
+
+
+def get_pi_ssh_dir(rootfs, create=True):
+    rootfs = check_path(rootfs)
+    pi_ssh_dir = rootfs / Path("home/pi/.ssh")
+    pi_ssh_dir.mkdir(exist_ok=True, parents=True)
+    return check_path(pi_ssh_dir)
+
+
+def get_sshd_dir(rootfs):
+    rootfs = check_path(rootfs)
+    filename = rootfs / "etc/ssh/sshd_config"
+    filename = check_path(filename)
+    return filename
+
+
+def disable_password_authentication(rootfs):
+    filename = get_sshd_dir(rootfs)
+    options = parse_sshd_conf(filename)
+    options = ["PasswordAuthentication no" if "PasswordAuthentication" in line else line for line in options]
+    filename.write_text("\n".join(options))
+
+
+def parse_sshd_conf(filename):
+    filename = check_path(filename)
+    options = filename.read_text().splitlines()
+    # print(options)
+    return options
+
+
+def ssh_addkey(rootfs, filename):
+    filename = check_path(filename)
+    pi_ssh_dir = get_pi_ssh_dir(rootfs)
+    authorized_keys = pi_ssh_dir / Path("authorized_keys")
+
+    new_key = filename.read_text()
+    all_keys = ""
+    if authorized_keys.exists():
+        all_keys = authorized_keys.read_text()
+    if new_key not in all_keys:
+        with authorized_keys.open("a") as f:
+            f.write(new_key)
+    print(f"added key {filename}")
+
+
+def parse_shadow_line(line):
+    values = line.split(":")
+    field_names = ["user", "password", "changed", "allowed_change",
+                   "required_change", "expiration_warning",
+                   "inactive", "expiration", "reserved"]
+    result = dict(zip(field_names, values))
+    return result["user"], result
+
+
+def get_shadow_file(rootfs):
+    filename = rootfs / "etc/shadow"
+    return check_path(filename)
+
+
+def load_shadow(rootfs):
+    filename = get_shadow_file(rootfs)
+    lines = filename.read_text().splitlines()
+    entries = dict([parse_shadow_line(e) for e in lines])
+    return entries
+
+
+def save_shadow(rootfs, entries):
+    filename = get_shadow_file(rootfs)
+    lines = [":".join(x.values()) for x in entries.values()]
+    filename.write_text("\n".join(lines))
+    return entries
+
+
+def change_pw(rootfs, password):
+    entries = load_shadow(rootfs)
+    pi_user = entries["pi"]
+
+    salt = crypt.mksalt()
+    hashed_pw = crypt.crypt(password, salt)
+
+    pi_user["password"] = hashed_pw
+    save_shadow(rootfs, entries)
+
+
+def change_host(hostname):
+    # change name in etc/hostname
+    # change name in etc/hosts
+    pass
+
+
 if __name__ == "__main__":
     # if os.geteuid() != 0:
     #     subprocess.call(['sudo', 'python3', *sys.argv])
     #     sys.exit()
 
-    devices = lsblk()
-    cards = filter_sd_cards(devices)
-    assert len(cards) == 1, "Error: more than one sd card found"
-    boot = get_mounts(cards[0], "boot")[0]
-    root_fs = get_mounts(cards[0], "rootfs")[0]
-    print(boot)
-    print(root_fs)
-    configure_wifi(boot, "test2", "test3")
-    configure_static_ip(root_fs)
+    # devices = lsblk()
+    # cards = filter_sd_cards(devices)
+    # assert len(cards) == 1, "Error: more than one sd card found"
+    # boot = get_mounts(cards[0], "boot")[0]
+    # root_fs = get_mounts(cards[0], "rootfs")[0]
+    # print(boot)
+    # print(root_fs)
+    # # configure_wifi(boot, "test2", "test3")
+    # # configure_static_ip(root_fs)
+    # pub_key = ssh_keygen("test123", "")
+    # ssh_addkey(root_fs, pub_key)
+    # disable_password_authentication()
+    change_pw("test")
